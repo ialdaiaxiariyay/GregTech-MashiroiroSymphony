599560270cb3d5f84881ece860d2d374c67e52f5
ialdaiaxiariyay <ialdaiaxiariyay@qq.com>
2026-02-12 16:04:25 +0800

    Mixin Placeholder

---
+++ src/main/java/top/ialdaiaxiariyay/gtms/mixin/BetterGTAEMixin.java
@@ -0,0 +1,7 @@
+package top.ialdaiaxiariyay.gtms.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import top.ialdaiaxiariyay.bettergtae.BetterGTAE;
+
+@Mixin(BetterGTAE.class)
+public class BetterGTAEMixin {}
